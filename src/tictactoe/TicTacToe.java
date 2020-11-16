08608b47dd715f806bbc659c3c69b1bad4de454d
Geddy Mukama <geddymaria.mukama@gmail.com>
2020-11-16 21:20:48 +1100

    initial commit, added grid rendering and whatnot. Will continue tomorrow.

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,32 @@
+package tictactoe;
+
+import java.lang.reflect.Array;
+import java.util.*;
+public class TicTacToe {
+	public static void main(String[] args) {
+		displayGrid(0,0, false);
+	}
+	public static void displayGrid(int x, int y, boolean p1Turn) {
+		//Hashtable of co-ords on the grid - to be rendered/interpreted:
+		Hashtable<Integer, Integer> coords = new Hashtable<Integer, Integer>();
+		
+		//Grid to render:
+		String[][] renderableGrid = {
+				{"  |", "   |", "   |"},
+				{"---", " --- ", "---"},
+				{"  |", "   |", "   |"},
+				{"---", " --- ", "---"},
+				{"  |", "   |", "   |"}
+		};
+		
+		//interpret grid here:
+		
+		//render grid
+		for (String[] gridY: renderableGrid) {
+			for (String gridX: gridY) {
+				System.out.print(gridX);
+			}
+			System.out.println();
+		}
+	}
+}
