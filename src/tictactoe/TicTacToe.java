f5b57a87d70a35b743182f90730ceb16def0286f
Geddy Mukama <geddymaria.mukama@gmail.com>
2020-11-20 13:53:50 +1100

    added input

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class TicTacToe {
 	public static void main(String[] args) {
 		displayGrid(0,0, false);
+		int[] input = getInput();
+		System.out.println(Arrays.toString(input));
 	}
 	public static void displayGrid(int x, int y, boolean p1Turn) {
 		//Hashtable of co-ords on the grid - to be rendered/interpreted:
@@ -12,11 +14,13 @@ public class TicTacToe {
 		
 		//Grid to render:
 		String[][] renderableGrid = {
-				{"  |", "   |", "   |"},
 				{"---", " --- ", "---"},
 				{"  |", "   |", "   |"},
 				{"---", " --- ", "---"},
-				{"  |", "   |", "   |"}
+				{"  |", "   |", "   |"},
+				{"---", " --- ", "---"},
+				{"  |", "   |", "   |"},
+				{"---", " --- ", "---"}
 		};
 		
 		//interpret grid here:
@@ -29,4 +33,16 @@ public class TicTacToe {
 			System.out.println();
 		}
 	}
+	
+	public static int[] getInput() {
+		int[] coordsInput = new int[2];
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter x coordinate:");
+		coordsInput[0] = in.nextInt();
+		System.out.println("Enter y coordinate:");
+		coordsInput[1] = in.nextInt();
+		
+		return coordsInput;
+	}
+	
 }
