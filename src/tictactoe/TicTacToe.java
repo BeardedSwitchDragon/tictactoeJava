58e365854d7a9b2faedf4e5e5caaf531d315b975
Geddy Mukama <geddymaria.mukama@gmail.com>
2020-11-25 18:34:40 +1100

    added horizontal win condition

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -38,45 +38,83 @@ public class TicTacToe {
 		//interpret grid here:
 		
 		//here i use 3x3 because a tictactoe grid will always be in these dimensions.
-		if (x <= 3) {
-			x -= 1;
-		}
-		if ((y % 2 == 0)) {
-			y += 1;
-		}
+		if (x <= 3) x -= 1;
+		
+		if (y == 3) y = 5;
+		
+		if ((y % 2 == 0)) y += 1;
+		
 		
 		coords[turn] = new int[] {x, y};
 		//update grid
 		
+		if (renderableGrid[y][x].contains("X") == false || renderableGrid[y][x].contains("O") == false ) {
 		
-		
-		if (p1Turn) {
-			
-			renderableGrid[y][x] = " X|";
+			if (p1Turn) {
+
+				switch (x) {
+				case 0:
+					renderableGrid[y][x] = " X|";
+					break;
+				case 1:
+					renderableGrid[y][x] = "  X|";
+					break;
+				case 2:
+					renderableGrid[y][x] = "  X|";
+					break;
+				default:
+					break;
+				}
+
+			} else {
+				renderableGrid[y][x] = " O|";
+			}
 		} else {
-			renderableGrid[y][x] = " O|";
+			System.out.println("Space is already occupied!");
 		}
 		//render grid
 		for (String[] gridY: renderableGrid) {
-			for (String gridX: gridY) {
+			for (String square: gridY) {
 				
 				
-				System.out.print(gridX);
+				System.out.print(square);
 			}
 			System.out.println();
 		}
+		gameDone = checkWinX();
 		turn++;
 	}
 	
 	public static int[] getInput() {
 		int[] coordsInput = new int[2];
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter x coordinate (1,1) is top left:");
-		coordsInput[0] = in.nextInt();
-		System.out.println("Enter y coordinate:");
+		System.out.println("Enter y coordinate (1,1) is top left:");
 		coordsInput[1] = in.nextInt();
+		System.out.println("Enter x coordinate:");
+		coordsInput[0] = in.nextInt();
 		turn++;
 		return coordsInput;
 	}
+	
+	public static boolean checkWinX() {
+		//check if horizontal win
+		boolean isMatched = true;
+		String winner;
+		for (String[] gridY: renderableGrid) {
+			
+			for (String square: gridY) {
+				//System.out.println(square.contains("X"));
+				isMatched = square.contains("X");
+			}
+			if (isMatched) {
+				System.out.println("winner is X");
+			}
+		}
+		System.out.println("wpllpl");
+		
+		return isMatched;
+	}
+	
+
 	
 }
